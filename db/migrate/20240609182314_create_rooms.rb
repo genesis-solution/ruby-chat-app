f81e033204339232b9d8b84988f425110918f4b0
genesis-solution <alex.ivanovic1001@gmail.com>
2024-09-19 13:46:47 +0800

    feat!: upgrade rooms db structure
    Break Changes: columns 'user1_id', 'user2_id' are added as foreign keys to rooms table

--- db/migrate/20240609182314_create_rooms.rb
+++ db/migrate/20240609182314_create_rooms.rb
@@ -3,6 +3,10 @@ class CreateRooms < ActiveRecord::Migration[7.1]
     create_table :rooms do |t|
       t.string :name
 
+      # Add two user foreign keys
+      t.references :user1, null: false, foreign_key: { to_table: :users }
+      t.references :user2, null: false, foreign_key: { to_table: :users }
+
       t.timestamps
     end
   end
